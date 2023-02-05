c90cd593f62dc64808a173609bd85eed4643464c
Brien-Austin <108070853+Brien-Austin@users.noreply.github.com>
2023-02-05 10:42:27 +0530

    Routed the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import {
-    BrowserRouter as Router, Routes, Route,
-  } from 'react-router-dom';
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Publicprofile from './pages/Publicprofile';
 import Sharedata from './pages/Sharedata';
 
 function App() {
-    return (
-        <div className="app">
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/sharedata" element={<Sharedata/>} />
-              <Route path="/publicprofile" element={<Publicprofile/>} />
-            </Routes>
-        </Router>
-        </div>
-  )
+    return ( <
+        div className = "app" >
+        <
+        Router >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Home / > }
+        /> <
+        Route path = "/sharedata"
+        element = { < Publicprofile / > }
+        /> <
+        Route path = "/publicprofile"
+        element = { < Sharedata / > }
+        /> <
+        /Routes> <
+        /Router> <
+        /div>
+    )
 }
 
 export default App
